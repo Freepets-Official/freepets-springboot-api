a19a045013386edb196a964222b006e44de6d202
Jinho622 <aceho21@gmail.com>
2026-08-09 15:04:47 +0900

    chore: Spring Boot 프로젝트 생성

---
+++ src/test/java/com/freepets/FreepetsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freepets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreepetsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

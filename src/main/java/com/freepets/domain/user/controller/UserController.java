1ac8c52b855ec2ec34f0490e9a6be02695c89bc2
Jinho622 <aceho21@gmail.com>
2026-08-16 22:44:48 +0900

    fix: 회원가입 API URI에 v1 경로 추가

--- src/main/java/com/freepets/domain/user/controller/UserController.java
+++ src/main/java/com/freepets/domain/user/controller/UserController.java
@@ -14,7 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
